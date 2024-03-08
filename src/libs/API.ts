32553db8a4175e0367ac9a31820a6649c2326fce
muharsadika <id.muharsadika@gmail.com>
2024-03-08 18:41:53 +0700

    fix: API RENDER link

--- src/libs/API.ts
+++ src/libs/API.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-	baseURL: import.meta.env.VITE_API_LOCALHOST,
+	baseURL: import.meta.env.VITE_API_RENDER,
 });
 
 export const SetAuthToken = (token: string) => {
