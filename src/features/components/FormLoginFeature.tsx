e662a558362bdc2de919e87ae351170ccf6dd0d8
muharsadika <id.muharsadika@gmail.com>
2024-06-22 21:23:33 +0700

    refactor: restyle login page

--- src/features/components/FormLoginFeature.tsx
+++ src/features/components/FormLoginFeature.tsx
@@ -1,66 +1,55 @@
-import { FormControl, Input, Text, Button, Box } from "@chakra-ui/react";
-import { FormLoginHook } from "../hooks/FormLoginHook";
-
+import { FormControl, Input, Text, Button, Box } from '@chakra-ui/react';
+import { FormLoginHook } from '../hooks/FormLoginHook';
 
 export default function FormLoginFeature() {
-	const { handleChange, handleLogin } = FormLoginHook();
+  const { handleChange, handleLogin } = FormLoginHook();
 
-	return (
-		<FormControl
-			isRequired
-			display={"flex"}
-			flexDirection={"column"}
-			gap={3}
-			width={"350px"}
-			bg={"transparent"}
-			color={"white"}
-			border={"1px solid white"}
-			borderRadius={10}
-			padding={5}
-		>
+  return (
+    <FormControl
+      isRequired
+      display={'flex'}
+      flexDirection={'column'}
+      gap={3}
+      width={'25%'}
+      bg={'blackAlpha.50'}
+      color={'white'}
+      border={'1px solid white'}
+      borderRadius={20}
+      padding={5}
+    >
+      <Text color={'blue.500'} fontSize={'9xl'} fontWeight={'bold'} marginTop={-50}>
+        Circle
+      </Text>
 
-			<Text color={"messenger.500"} fontSize={"3xl"} fontWeight={"bold"}>
-				Circle
-			</Text>
+      <Text fontSize={'2xl'} fontWeight={'bold'} display={'flex'} gap={1}>
+        <Text>Login</Text>
+        <Text color="blue.500">Circle</Text>
+      </Text>
 
-			<Text fontSize={"2xl"} fontWeight={"bold"} display={"flex"} gap={1}>
-				<Text>Login</Text>
-				<Text color="messenger.500">Circle</Text>
-			</Text>
+      <Input placeholder="Username" name="username" onChange={handleChange} />
 
-			<Input
-				placeholder="Username"
-				name="username"
-				onChange={handleChange}
-			/>
+      <Input
+        type="password"
+        placeholder="Password"
+        name="password"
+        onChange={handleChange}
+        onKeyDown={handleLogin}
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            handleLogin();
+          }
+        }}
+      />
 
-			<Input
-				type="password"
-				placeholder="Password"
-				name="password"
-				onChange={handleChange}
-				onKeyDown={handleLogin}
-				onKeyPress={(event) => {
-					if (event.key === "Enter") {
-						handleLogin();
-					}
-				}}
-			/>
+      <Box display="flex" justifyContent={'flex-end'}>
+        <Text color="blue.700" cursor={'pointer'} _hover={{ color: 'blue.500' }}>
+          Forgot password?
+        </Text>
+      </Box>
 
-			<Box display="flex" justifyContent={"flex-end"}>
-
-				<Text>Forgot password?</Text>
-
-			</Box>
-
-			<Button
-				backgroundColor={"messenger.500"}
-				colorScheme="messenger.500"
-				color={"white"}
-				onClick={handleLogin}>
-				Login
-			</Button>
-
-		</FormControl>
-	);
+      <Button colorScheme="blue" onClick={handleLogin}>
+        Login
+      </Button>
+    </FormControl>
+  );
 }
