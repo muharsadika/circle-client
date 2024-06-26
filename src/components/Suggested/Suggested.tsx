7ed0bc003f4d4eb1235ed7f440a28d40cbf1e87b
muharsadika <id.muharsadika@gmail.com>
2024-06-26 20:12:49 +0700

    fix: refactoring and fixing layout and function in card suggest

--- src/components/Suggested/Suggested.tsx
+++ src/components/Suggested/Suggested.tsx
@@ -1,37 +1,46 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Box, Card, Stack, Text } from "@chakra-ui/react";
-import SuggestedFeature from "@/features/components/SuggestedFeature";
-import { useGetUsers } from "@/features/hooks/GetUsersHook";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/type/RootState";
+import { Card, CardBody, CardHeader, Heading, Stack } from '@chakra-ui/react';
+import SuggestedFeature from '@/features/components/SuggestedFeature';
+import { useGetUsers } from '@/features/hooks/GetUsersHook';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store/type/RootState';
+
+interface IUser {
+  id: number;
+  username: string;
+  full_name: string;
+  profile_picture: string;
+}
 
 export default function SuggestedComponent() {
-    const { GetUsers, isLoading } = useGetUsers();
-    const signInUser = useSelector((state: RootState) => state?.auth);
+  const { GetUsers, isLoading } = useGetUsers();
+  const signInUser = useSelector((state: RootState) => state?.auth);
 
-    const List = GetUsers?.filter((user: any) => user.id !== signInUser?.id);
+  // Filter user yang ditampilkan pada card suggest hanya 5 user saja kecuali yang sedang login
+  const List = GetUsers?.filter((user: IUser) => user.id !== signInUser?.id).slice(0, 5);
 
-    if (isLoading) return <div>Loading...</div>;
-    return (
-        <Card
-            bg="whiteAlpha.200"
-            p={4} height={"!00%"}
-            // border={"1px solid blue"}
-        >
-            <Text color="white">Suggested for you</Text>
-            <Box mt={3}  >
-                <Stack >
-                    {List.map((user: any) => (
-                        <SuggestedFeature
-                            key={user.id}
-                            user_id={user.id}
-                            username={user.username}
-                            full_name={user.full_name}
-                            profile_picture={user.profile_picture}
-                        />
-                    ))}
-                </Stack>
-            </Box>
-        </Card>
-    );
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <Card bg="whiteAlpha.100" rounded={'2xl'}>
+      <CardHeader>
+        <Heading color={'white'} size={'xs'} fontWeight={'medium'}>
+          Suggested for you
+        </Heading>
+      </CardHeader>
+
+      <CardBody>
+        <Stack gap={5}>
+          {List.map((user: IUser) => (
+            <SuggestedFeature
+              key={user.id}
+              user_id={user.id}
+              username={user.username}
+              full_name={user.full_name}
+              profile_picture={user.profile_picture}
+            />
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
+  );
 }
