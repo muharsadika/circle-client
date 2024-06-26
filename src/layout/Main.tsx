c8865d407d4cf3046bbbe0afc06f3b8a22d18568
muharsadika <id.muharsadika@gmail.com>
2024-06-26 16:48:45 +0700

    refactor: changing Grid to Stack

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,12 +1,13 @@
 import NavbarComponent from '@/components/Navbar/Navbar';
 import SuggestedComponent from '@/components/Suggested/Suggested';
 import FooterComponent from '@/components/Footer/Footer';
-import { Box, Grid, GridItem } from '@chakra-ui/react';
+import { Box, Stack } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import ProfileFeature from '../components/Profile/Profile';
 
 export default function Main({ children }: { children: ReactNode }) {
   return (
+    <>
       <Box display={'flex'} h={'100vh'} my={1}>
         <Box w={'25%'}>
           <NavbarComponent />
@@ -15,18 +16,13 @@ export default function Main({ children }: { children: ReactNode }) {
         <Box w={'50%'}>{children}</Box>
 
         <Box w={'25%'}>
-          <Grid gap={5} w={'90%'} mx={'auto'}>
-            <GridItem>
-              <ProfileFeature />
-            </GridItem>
-            <GridItem>
-              <SuggestedComponent />
-            </GridItem>
-            <GridItem>
-              <FooterComponent />
-            </GridItem>
-          </Grid>
+          <Stack gap={5} w={'90%'} mx={'auto'}>
+            <ProfileFeature />
+            <SuggestedComponent />
+            <FooterComponent />
+          </Stack>
         </Box>
       </Box>
+    </>
   );
 }
