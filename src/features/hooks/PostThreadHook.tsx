d4aba5b920485acb966b84db7e20f83b218a8d97
muharsadika <id.muharsadika@gmail.com>
2024-06-30 20:07:11 +0700

    feat(hook-post-thread): changing handle post adapt with textarea and textarea height can adapt the content

--- src/features/hooks/PostThreadHook.tsx
+++ src/features/hooks/PostThreadHook.tsx
@@ -1,87 +1,100 @@
-import { FormThreadType } from "@/Types/ThreadType";
-import { API } from "@/libs/API";
-import { RootState } from "@/store/type/RootState";
-import { useDisclosure } from "@chakra-ui/react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { ChangeEvent, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import { FormThreadType } from '@/Types/ThreadType';
+import { API } from '@/libs/API';
+import { RootState } from '@/store/type/RootState';
+import { useDisclosure } from '@chakra-ui/react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { ChangeEvent, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 export function usePostThread() {
-	const [form, setForm] = useState<FormThreadType>({
-		content: "",
-	});
+  const [form, setForm] = useState<FormThreadType>({
+    content: '',
+  });
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-	const QueryClient = useQueryClient();
-	const fileInputRef = useRef<HTMLInputElement>(null);
+  const QueryClient = useQueryClient();
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
-	function handleButtonClick() {
-		fileInputRef.current?.click();
-	}
+  function handleButtonClick() {
+    fileInputRef.current?.click();
+  }
 
-	const [image, setImage] = useState<File | null>(null);
+  const [image, setImage] = useState<File | null>(null);
 
-	function handleChange(e: ChangeEvent<HTMLInputElement>) {
-		setForm({
-			...form,
-			[e.target.name]: e.target.files ? e.target.files : e.target.value,
-		});
-	}
+  // function handleChange(e: ChangeEvent<HTMLInputElement>) {
+  // 	setForm({
+  // 		...form,
+  // 		[e.target.name]: e.target.files ? e.target.files : e.target.value,
+  // 	});
+  // }
 
-	const { mutate: handlePost, isPending } = useMutation({
-		mutationFn: async () => {
-			const formData = new FormData();
-			if (image) {
-				formData.append("image", image);
-			}
-			formData.append("content", form.content);
-			await API.post("/thread", formData);
-		},
-		onSuccess: () => {
-			QueryClient.invalidateQueries({ queryKey: ["thread"] });
-			setForm({
-				content: "",
-			});
-			setImage(null);
-		},
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value, files } = e.target as HTMLInputElement & HTMLTextAreaElement;
+    setForm({
+      ...form,
+      [name]: files ? files : value,
+    });
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  };
 
-	});
+  const { mutate: handlePost, isPending } = useMutation({
+    mutationFn: async () => {
+      const formData = new FormData();
+      if (image) {
+        formData.append('image', image);
+      }
+      formData.append('content', form.content);
+      await API.post('/thread', formData);
+    },
+    onSuccess: () => {
+      QueryClient.invalidateQueries({ queryKey: ['thread'] });
+      setForm({
+        content: '',
+      });
+      setImage(null);
+    },
+  });
 
-	const { isOpen, onOpen, onClose } = useDisclosure();
-	
-	const user = useSelector((state: RootState) => state.auth);
-	
-	// async function handlePost() {
-	// 	// setIsPosting(true);
-	// 	try {
-	// 		console.log(form);
-	// 		const formData = new FormData();
-	// 		if (image) {
-	// 			formData.append("image", image);
-	// 		}
-	// 		formData.append("content", form.content);
-	// 		await API.post("/thread", formData);
-	// 		// refetch();
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	} finally {
-	// 		// setIsPosting(false);
-	// 		setForm({
-	// 			content: "",
-	// 		});
-	// 		setImage(null);
-	// 	}
-	// }
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-	return {
-		handleButtonClick,
-		handleChange,
-		handlePost,
-		fileInputRef,
-		isPending,
-		setImage,
-		isOpen,
-		onOpen,
-		onClose,
-		user
-	};
+  const user = useSelector((state: RootState) => state.auth);
+
+  // async function handlePost() {
+  // 	// setIsPosting(true);
+  // 	try {
+  // 		console.log(form);
+  // 		const formData = new FormData();
+  // 		if (image) {
+  // 			formData.append("image", image);
+  // 		}
+  // 		formData.append("content", form.content);
+  // 		await API.post("/thread", formData);
+  // 		// refetch();
+  // 	} catch (error) {
+  // 		console.log(error);
+  // 	} finally {
+  // 		// setIsPosting(false);
+  // 		setForm({
+  // 			content: "",
+  // 		});
+  // 		setImage(null);
+  // 	}
+  // }
+
+  return {
+    handleButtonClick,
+    handleChange,
+    handlePost,
+    fileInputRef,
+    isPending,
+    setImage,
+    isOpen,
+    onOpen,
+    onClose,
+    user,
+    textareaRef,
+  };
 }
