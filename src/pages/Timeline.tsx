2a77efad36c58f6fb4f165a35bf2a4169d5e75f0
muharsadika <id.muharsadika@gmail.com>
2024-07-02 14:49:04 +0700

    fix(timeline): fixing layout timeline

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Stack, Text } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 import ThreadFeature from '@/features/components/ThreadFeature';
 import { ThreadType } from '@/Types/ThreadType';
 import { useGetThread } from '@/features/hooks/GetThreadHook';
@@ -6,40 +6,69 @@ import FormThreadFeature from '@/features/components/FormThreadFeature';
 
 function Timeline() {
   const { getThread } = useGetThread();
+  console.log('getThread', getThread);
 
   return (
     <>
-      <Grid
-        overflowY="auto"
-        // padding={3}
-      >
-        <GridItem py={3}>
-          <Text color="white" fontSize="lg" align={'center'}>
-            Home
-          </Text>
-        </GridItem>
+      <Box w={'90%'} mx={'auto'} borderX={'1px solid rgb(255, 255, 255, 0.5)'}>
+        <Box w={'100%'} h={10} display={'flex'}>
+          <Box
+            w={'50%'}
+            h={'100%'}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            _hover={{
+              cursor: 'pointer',
+              bgColor: 'rgb(125, 125, 125, 0.1)',
+            }}
+            bgColor={
+              window.location.href.includes('home') ? 'rgb(125, 125, 125, 0.1)' : 'transparent'
+            }
+          >
+            <Heading color={'white'} fontSize={'sm'} textAlign={'center'} fontWeight={'medium'}>
+              For you
+            </Heading>
+          </Box>
+          <Box
+            w={'50%'}
+            h={'100%'}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            _hover={{
+              cursor: 'pointer',
+              bgColor: 'rgb(125, 125, 125, 0.1)',
+            }}
+            bgColor={
+              window.location.href.includes('following') ? 'rgb(125, 125, 125, 0.1)' : 'transparent'
+            }
+          >
+            <Heading color={'white'} fontSize={'sm'} textAlign={'center'} fontWeight={'medium'}>
+              Following
+            </Heading>
+          </Box>
+        </Box>
 
-        <GridItem>
+        <Box borderY={'1px solid rgb(255, 255, 255, 0.5)'}>
           <FormThreadFeature />
-        </GridItem>
+        </Box>
 
-        <GridItem>
-          <Stack>
-            {getThread &&
-              getThread?.map((e: ThreadType) => (
-                <ThreadFeature
-                  key={e.id}
-                  id={e.id}
-                  content={e.content}
-                  image={e.image}
-                  user={e.user}
-                  replies={e.replies}
-                  likes={e.likes}
-                />
-              ))}
-          </Stack>
-        </GridItem>
-      </Grid>
+        <Box>
+          {getThread &&
+            getThread?.map((e: ThreadType) => (
+              <ThreadFeature
+                key={e.id}
+                id={e.id}
+                content={e.content}
+                image={e.image}
+                user={e.user}
+                replies={e.replies}
+                likes={e.likes}
+              />
+            ))}
+        </Box>
+      </Box>
     </>
   );
 }
